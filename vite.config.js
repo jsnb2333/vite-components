733af3a7a4a7708ea52dddb7e93cd81b87821a44
jsnb2333 <1357930097@qq.com>
2023-07-08 23:15:17 +0800

    test:不发布dist是否可用

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'lib/main.js'),
       // 打包为umd和iife需要设置的字段，提供包名。
-      name: 'MyLib',
+      name: '@jsnb2333/components',
       // 输出的库文件名
-      fileName: 'my-lib'
+      fileName: 'components'
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
